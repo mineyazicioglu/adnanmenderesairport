2b9d27ceba2e4d30cbb80f26b1667c44bd9193c2
Mine <mine.bilgic@gmail.com>
2017-09-22 18:28:45 +0300

    Added CoffeeScript Files

--- gulpfile.js
+++ gulpfile.js
@@ -1,6 +1,15 @@
 var gulp = require('gulp'),
     gutil = require('gulp-util');
 
-gulp.task('log', function() {
+/*gulp.task('log', function() {
 	gutil.log('Airport application rocks!');
+});*/
+
+var coffeeSoures = ['components/coffee/logo.coffee'];
+
+gulp.task('coffee', function() {
+	gulp.src(coffeeSoures)
+	.pipe(coffee({bare: true})
+		.on('error', gutil.log))
+	.pipe(gulp.dest('components/scripts'))
 });
