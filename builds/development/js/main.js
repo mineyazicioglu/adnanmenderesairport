aaee7f84fa49eef425dfb6aaf2fe82ec4b56bf3f
Mine <mine.bilgic@gmail.com>
2017-09-22 18:51:05 +0300

    Concatenated JavaScript Files

--- builds/development/js/main.js
+++ builds/development/js/main.js
@@ -1,1 +1,10 @@
+$(function() {
+  var Mustache = require('mustache');
 
+  $.getJSON('js/data.json', function(data) {
+    var template = $('#newstpl').html();
+    var html = Mustache.to_html(template, data);
+    $('#news').html(html);    
+  }); //getJSON
+  
+}); //function
--- gulpfile.js
+++ gulpfile.js
@@ -1,15 +1,25 @@
 var gulp = require('gulp'),
     gutil = require('gulp-util');
+    coffee = require('gulp-coffee');
+    concat = require('gulp-concat');
 
 /*gulp.task('log', function() {
 	gutil.log('Airport application rocks!');
 });*/
 
-var coffeeSoures = ['components/coffee/logo.coffee'];
+var coffeeSources = ['components/coffee/logo.coffee'];
+var jsSources = ['components/scripts/template.js'];
 
 gulp.task('coffee', function() {
-	gulp.src(coffeeSoures)
+	gulp.src(coffeeSources)
 	.pipe(coffee({bare: true})
 		.on('error', gutil.log))
 	.pipe(gulp.dest('components/scripts'))
+});
+
+
+gulp.task('js', function() {
+	gulp.src(jsSources)
+	.pipe(concat('main.js'))
+	.pipe(gulp.dest('builds/development/js'))
 });
